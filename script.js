0e2d9ba07a1adba4f2c2fd8f8a3737a2af0c3448
Paulo <00001076531623sp@al.educacao.sp.gov.br>
2025-02-26 19:48:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.getElementById('login-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('login').value;
+    const password = document.getElementById('senha').value;
 
+    // Login simples e estático (use um sistema de autenticação no servidor para produção)
+    const validUsername = 'paulo';
+    const validPassword = '123';
+
+    if (username === validUsername && password === validPassword) {
+        document.querySelector('.login-container').style.display = 'none';
+        document.getElementById('conteudo-protegido').style.display = 'block';
+    } else {
+        document.getElementById('error-message').style.display = 'block';
+    }
+});
